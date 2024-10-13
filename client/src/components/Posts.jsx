eacdd079f9c9e941b062a94834e2a62b2f1fa7cf
SHANIBJAN <shanib@SHANIBJANs-MacBook-Pro.local>
2024-10-12 20:30:35 -0700

    down navbar added

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -9,6 +9,9 @@ import {
   faCommentDots,
   faPaperPlane,
   faBookmark,
+  faSquarePlus,
+  faMessage,
+  faBell,
 } from "@fortawesome/free-regular-svg-icons";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import feed from "../images/br.jpg";
@@ -184,6 +187,20 @@ const Posts = () => {
         </div>
       )}
       </AnimatePresence>
+      <div className=" fixed bottom-0 w-full bg-white px-[5%] py-[2%] hidden max-[800px]:flex justify-between " >  
+        <div className="h-[25px]" >
+          <FontAwesomeIcon icon={faSquarePlus} className="w-full h-full" />
+        </div>
+        <div className="h-[25px]" >
+          <FontAwesomeIcon icon={faMessage} className="w-full h-full" />
+        </div>
+        <div className="h-[25px]" >
+          <FontAwesomeIcon icon={faBell} className="w-full h-full" />
+        </div>
+        <div className="h-[25px]" >
+          <img className="h-full" src={user} alt="" />
+        </div>
+      </div>
       
     </div>
   );
